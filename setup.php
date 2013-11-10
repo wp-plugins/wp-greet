bb5a8e1844c165f651bb53144c4bf193c1d3fc9f
tuxlog <tuxlog@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-10 18:52:20 +0000

    wp-greet v3.7. tag
    git-svn-id: https://plugins.svn.wordpress.org/wp-greet/tags/3.7@802110 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- setup.php
+++ setup.php
@@ -1,7 +1,7 @@
 <?php
 /* This file is part of the wp-greet plugin for wordpress */
 
-/*  Copyright 2008,2009  Hans Matzen  (email : webmaster at tuxlog.de)
+/*  Copyright 2008-2013  Hans Matzen  (email : webmaster at tuxlog.de)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -26,22 +26,38 @@ are not allowed to call this page directly.'); }
 //
 function wp_greet_activate()
 {
-  global $wpdb, $wp_roles, $wp_version,$wpg_options;
-  
-  // upgrade function changed in WordPress 2.3	
-  if (version_compare($wp_version, '2.3', '>='))		
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-  else
-    require_once(ABSPATH . 'wp-admin/upgrade-functions.php');
-
-  $wpg_options = wpgreet_get_options();
-
-  debug("<pre>###".WP_GREET_VERSION."###".$wpg_options['wp-greet-version']."###</pre>");
-  debug(version_compare(WP_GREET_VERSION,$wpg_options['wp-greet-version'], '>'));
-  if ($wpg_options['wp-greet-version'] == "") {
+    global $wpdb, $wp_roles, $wp_version,$wpg_options;
+    
+    // upgrade function changed in WordPress 2.3	
+    if (version_compare($wp_version, '2.3', '>='))		
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    else
+	require_once(ABSPATH . 'wp-admin/upgrade-functions.php');
+    
+    $wpg_options = wpgreet_get_options();
+    
+    
     $wpg_options['wp-greet-version'] = WP_GREET_VERSION;
-    add_option("wp-greet-version",$wpg_options['wp-greet-version'],
-		 "versionnumber of the installed wp-greet","yes");
+    update_option("wp-greet-version",$wpg_options['wp-greet-version'],
+		  "versionnumber of the installed wp-greet","yes");
+    
+    // retrieve collation
+    //Use the character set and collation that's configured for WP tables
+	$charset_collate = '';
+	if ( !empty($wpdb->charset) ){
+		//Some German installs use "utf-8" (invalid) instead of "utf8" (valid). None of 
+		//the charset ids supported by MySQL contain dashes, so we can safely strip them.
+		//See http://dev.mysql.com/doc/refman/5.0/en/charset-charsets.html 
+		$charset = str_replace('-', '', $wpdb->charset);
+		
+		$charset_collate = "DEFAULT CHARACTER SET {$charset}";
+	}
+	if ( !empty($wpdb->collate) ){
+		$charset_collate .= " COLLATE {$wpdb->collate}";
+	}
+    
+    
+    
     // create table	
     $sql = "CREATE TABLE " . $wpdb->prefix . 'wpgreet_stats' . " (
 	    mid BIGINT NOT NULL AUTO_INCREMENT ,
@@ -51,112 +67,231 @@ function wp_greet_activate()
             picture VARCHAR(255) NOT NULL,
             mailbody MEDIUMTEXT NOT NULL,
             remote_ip VARCHAR(15) NOT NULL,
-	    PRIMARY KEY mid (mid)
-	    );";
-	
-      dbDelta($sql);
-
-  } else if (version_compare(WP_GREET_VERSION, 
-			     $wpg_options['wp-greet-version'], '>') ) {
-
-    // add column for ip logging (since v1.2)
-    $sql="alter table " . $wpdb->prefix . "wpgreet_stats add column remote_ip varchar(15) NOT NULL after mailbody;";
-    $wpdb->query($sql);
-
-    // neue version wegschreiben
-    $wpg_options['wp-greet-version'] = WP_GREET_VERSION;
-    wpgreet_set_options();
-  }
+	    PRIMARY KEY  (mid)
+	    ) $charset_collate ;";
     
+    dbDelta($sql);
+    
+    // create table for greet card store (since v1.7)	
+    $sql = "CREATE TABLE " . $wpdb->prefix . 'wpgreet_cards' . " (
+	    mid BIGINT NOT NULL AUTO_INCREMENT ,
+            fromname VARCHAR(80) NOT NULL,
+	        frommail VARCHAR(80) NOT NULL,
+            toname VARCHAR(80) NOT NULL,
+            tomail VARCHAR(80) NOT NULL,
+            cc2from smallint NOT NULL,
+            subject VARCHAR(255) NOT NULL,
+            picture VARCHAR(255) NOT NULL,
+            mailbody MEDIUMTEXT NOT NULL,
+            confirmuntil timestamp NOT NULL,
+            confirmcode VARCHAR(32) NOT NULL,
+            fetchuntil timestamp NOT NULL,
+            fetchcode VARCHAR(32) NOT NULL,
+            card_sent timestamp NOT NULL,
+	        card_fetched timestamp NOT NULL,
+	        future_send timestamp NOT NULL,
+	        session_id VARCHAR(32) NOT NULL,
+            PRIMARY KEY  (mid)
+	    ) $charset_collate ;";
+    
+    dbDelta($sql);
+    
+
     if ($wpg_options['wp-greet-minseclevel'] == "") {
-      $wpg_options['wp-greet-minseclevel'] = 'Registered';
-      add_option("wp-greet-minseclevel",$wpg_options['wp-greet-minseclevel'],
-		 "the minimum security level to send greeting cards","yes");
+	$wpg_options['wp-greet-minseclevel'] = 'Registered';
+	add_option("wp-greet-minseclevel",$wpg_options['wp-greet-minseclevel'],'',"yes");
     };
     
     
     if ($wpg_options['wp-greet-captcha'] == "") {
-      $wpg_options['wp-greet-captcha'] = 0;
-      add_option("wp-greet-captcha",$wpg_options['wp-greet-captcha'],
-		 "wanna use a captcha to prevent spamming?","yes");
+	$wpg_options['wp-greet-captcha'] = 0;
+	add_option("wp-greet-captcha",$wpg_options['wp-greet-captcha'],'',"yes");
     }; 
     
     if ($wpg_options['wp-greet-mailreturnpath'] == "") {
-      $wpg_options['wp-greet-mailreturnpath'] = "";
-      add_option("wp-greet-mailreturnpath",$wpg_options['wp-greet-mailreturnpath'],"the standard mail return path","yes");
+	$wpg_options['wp-greet-mailreturnpath'] = "";
+	add_option("wp-greet-mailreturnpath",$wpg_options['wp-greet-mailreturnpath'],'',"yes");
     }; 
-
+     
+    if ($wpg_options['wp-greet-staticsender'] == "") {
+	$wpg_options['wp-greet-staticsender'] = "";
+	add_option("wp-greet-staticsender",$wpg_options['wp-greet-staticsender'],'',"yes");
+    }; 
+    
     if ($wpg_options['wp-greet-autofillform'] == "") {
-      $wpg_options['wp-greet-autofillform'] = 1;
-      add_option("wp-greet-autofillform",$wpg_options['wp-greet-autofillform'],
-		 "try to fill form with user data","yes");
+	$wpg_options['wp-greet-autofillform'] = 1;
+	add_option("wp-greet-autofillform",$wpg_options['wp-greet-autofillform'],'',"yes");
+    };
+    
+    if ($wpg_options['wp-greet-imagewidth'] == "") {
+	$wpg_options['wp-greet-imagewidth'] = 400;
+	add_option("wp-greet-imagewidth",$wpg_options['wp-greet-imagewidth'],'',"yes");
+    };
+
+    if ($wpg_options['wp-greet-stampimage'] == "") {
+	$wpg_options['wp-greet-stampimage'] = "wp-content/plugins/wp-greet/defaultstamp.jpg";
+	add_option("wp-greet-stampimage",$wpg_options['wp-greet-stampimage'],'',"yes");
+    };
+
+    if ($wpg_options['wp-greet-stamppercent'] == "") {
+	$wpg_options['wp-greet-stamppercent'] = 15;
+	add_option("wp-greet-stamppercent",$wpg_options['wp-greet-stamppercent'],'',"yes");
+    };
+
+    if ($wpg_options['wp-greet-logging'] == "") {
+	$wpg_options['wp-greet-logging'] = 1;
+	add_option("wp-greet-logging",$wpg_options['wp-greet-logging'],'',"yes");
+    };
+    
+    if ($wpg_options['wp-greet-gallery'] == "") {
+	$wpg_options['wp-greet-gallery'] = "ngg";
+	add_option("wp-greet-gallery",$wpg_options['wp-greet-gallery'],'',"yes");
+    };
+    
+    if ($wpg_options['wp-greet-linesperpage'] == "") {
+	$wpg_options['wp-greet-linesperpage'] = "10";
+	add_option("wp-greet-linesperpage",$wpg_options['wp-greet-linesperpage'],'',"yes");
+    }; 
+    
+    if ($wpg_options['wp-greet-usesmtp'] == "") {
+	$wpg_options['wp-greet-usesmtp'] = "1";
+	add_option("wp-greet-usesmtp",$wpg_options['wp-greet-usesmtp'],'',"yes");
+    };
+    
+    if ($wpg_options['wp-greet-touswitch'] == "") {
+	$wpg_options['wp-greet-touswitch'] = "1";
+	add_option("wp-greet-touswitch",$wpg_options['wp-greet-touswitch'],'',"yes");
+    }; 
+    
+    if ($wpg_options['wp-greet-termsofusage'] == "") {
+	$wpg_options['wp-greet-termsofusage'] = "<h2>Grußkarten (eCard) System: Nutzungsbedinungen- und Hinweise</h2>
+<p>Bevor Sie eine Grußkarte absenden können, müssen Sie die Grußkarte bestätigen. Dazu erhalten Sie nach absenden der Grußkarte eine Email, die einen Bestätigungslink enthält. Erst nach klicken dieses Links wird Ihre Grußkarte tatsächlich abgeschickt.</p>
+
+<p>Mit Absenden Ihrer eCard haben Sie nachfolgende Hinweise gelesen und bestätigt.
+Der eCard Versender stellt sicher, dass er bei der Nutzung unseres eCard Service nicht gegen geltende Rechtsvorschriften verstößt. Der Versender stellt ebenso sicher und bestätigt, vor versenden seiner eCard, dass die Empfänger der verschickten Nachrichten mit dem Empfang der Nachricht einverstanden sind. Die Versendung kommerzieller Werbe Mails über den Grußkartendienst wird hiermit ausdrücklich untersagt. Ebenfalls ausdrücklich untersagt ist die Versendung von unerwünschten Mails (SPAM) oder Massen-Mails.</p>
+
+<p>Das Urheberrecht aller eCards liegt beim Betreiber dieser Site. Die Genehmigung zum Versenden der eCards zwecks Grußkartenübermittlung ist hiermit erteilt. Jegliche weitere Nutzung bedarf einer schriftlichen Genehmigung.</p>
+
+<p>Datenschutz: Ihre eMail-Adresse und die eMail-Adresse des Empfängers werden ausschließlich zu Übertragungszwecken verwendet . Zum Einen um Ihnen die Möglichkeit zu geben die Karte als Absender per Email zu bestätigen zum Anderen um den Empfänger über die eCard zu informieren, bzw. Sie, als Absender einer eCard, bei einem Fehler beim Versenden, zu benachrichtigen.</p>";
+	add_option("wp-greet-termsofusage",$wpg_options['wp-greet-termsofusage'],'', "yes");
+    };   
+    
+    if ($wpg_options['wp-greet-mailconfirm'] == "") {
+	$wpg_options['wp-greet-mailconfirm'] = "0";
+	add_option("wp-greet-mailconfirm",$wpg_options['wp-greet-mailconfirm'],'', "yes");
+    }; 
+    
+    if ($wpg_options['wp-greet-mcduration'] == "") {
+	$wpg_options['wp-greet-mcduration'] = "0";
+	add_option("wp-greet-mcduration",$wpg_options['wp-greet-mcduration'],'', "yes");
+    };
+    
+    if ($wpg_options['wp-greet-mctext'] == "") {
+	$wpg_options['wp-greet-mctext'] = "Hallo %sender%,
+bitte bestätigen Sie, dass Sie %sender% (%sendermail%) eine Grußkarte senden möchten, indem Sie auf den unten stehenden Link klicken.
+
+Bestätigungslink: %link%
+
+Wenn Sie nicht wissen warum Sie diese E-Mail erhalten oder die Postkarte nicht absenden möchten, dann ignorieren Sie diese E-Mail oder kontaktieren den Administrator, indem Sie einfach auf diese E-Mail antworten.
+
+Mit freundlichen Grüßen
+wp-greet, das freundliche Wordpress Grußkartenplugin";
+	add_option("wp-greet-mctext",$wpg_options['wp-greet-mctext'],'', "yes");
+    };  
+
+    if ($wpg_options['wp-greet-ectext'] == "") {
+	$wpg_options['wp-greet-ectext'] = "Hallo %sender% (%sendermail%),
+Ihre Grusskarte wurde unter dem %link% abgeholt. Vielen Dank, dass Sie den Grusskartendienst von wp-greet genutzt haben.
+
+Wenn Sie nicht wissen warum Sie diese E-Mail erhalten oder die Postkarte nicht absenden möchten, dann ignorieren Sie diese E-Mail oder kontaktieren den Administrator, indem Sie einfach auf diese E-Mail antworten.
+
+Mit freundlichen Grüßen
+wp-greet, das freundliche Wordpress Grußkartenplugin";
+	add_option("wp-greet-ectext",$wpg_options['wp-greet-ectext'], '',"yes");
+    };  
+    
+    if ($wpg_options['wp-greet-onlinecard'] == "") {
+	$wpg_options['wp-greet-onlinecard'] = "0";
+	add_option("wp-greet-onlinecard",$wpg_options['wp-greet-onlinecard'],'',"yes");
+    };  
+    
+    if ($wpg_options['wp-greet-ocduration'] == "") {
+	$wpg_options['wp-greet-ocduration'] = "0";
+	add_option("wp-greet-ocduration",$wpg_options['wp-greet-ocduration'],'',"yes");
+    }; 
+    
+    if ($wpg_options['wp-greet-octext'] == "") {
+	$wpg_options['wp-greet-octext'] = "Hallo %receiver%,\n\n Sie haben eine Grußkarte von %sender% erhalten. Die Grußkarte kann unter %link% abgeholt werden.\n
+
+Die Postkarte steht für Sie für die nächsten %duration% Tage zur Abholung bereit.
+
+Mit freundlichen Grüßen
+wp-greet, das freundliche Wordpress Grußkartenplugin";
+	add_option("wp-greet-octext",$wpg_options['wp-greet-octext'],'',"yes");
+    };   
+    
+    if ($wpg_options['wp-greet-logdays'] == "") {
+	$wpg_options['wp-greet-logdays'] = "0";
+	add_option("wp-greet-logdays",$wpg_options['wp-greet-logdays'],'',"yes");
+    };
+    
+    if ($wpg_options['wp-greet-carddays'] == "") {
+	$wpg_options['wp-greet-carddays'] = "0";
+	add_option("wp-greet-carddays",$wpg_options['wp-greet-carddays'],'', "yes");
     };
    
-    if ($wpg_options['wp-greet-imagewidth'] == "") {
-      $wpg_options['wp-greet-imagewidth'] = 400;
-      add_option("wp-greet-imagewidth",$wpg_options['wp-greet-imagewidth'],
-		 "gives the fixe image width","yes");
+    if ($wpg_options['wp-greet-fields'] == "") {
+	$wpg_options['wp-greet-fields'] = "010100"; // sender and receiver email is allways mandatory
+	add_option("wp-greet-fields",$wpg_options['wp-greet-fields'], '', "yes");
     };
-    if ($wpg_options['wp-greet-stampimage'] == "") {
-	$wpg_options['wp-greet-stampimage'] = "defaultstamp.jpg";
-      add_option("wp-greet-stampimage",$wpg_options['wp-greet-stampimage'],
-		 "file name of stamp-image","yes");
+    
+    if ($wpg_options['wp-greet-tinymce'] == "") {
+    	$wpg_options['wp-greet-tinymce'] = "1"; // use TinyMCE editor in form
+    	add_option("wp-greet-tinymce",$wpg_options['wp-greet-fields'], '', "yes");
     };
-    if ($wpg_options['wp-greet-stamppercent'] == "") {
-      $wpg_options['wp-greet-stamppercent'] = 15;
-      add_option("wp-greet-stamppercent",$wpg_options['wp-greet-stamppercent'],
-		 "gives the stamp width in percent of imagewidth","yes");
-    };
-    if ($wpg_options['wp-greet-logging'] == "") {
-      $wpg_options['wp-greet-logging'] = 1;
-      add_option("wp-greet-logging",$wpg_options['wp-greet-logging'],
-		 "enable logging","yes");
-    };
-    if ($wpg_options['wp-greet-gallery'] == "") {
-      $wpg_options['wp-greet-gallery'] = "ngg";
-      add_option("wp-greet-gallery",$wpg_options['wp-greet-gallery'],
-		 "which gallery to use","yes");
-    };
-
-    if ($wpg_options['wp-greet-linesperpage'] == "") {
-      $wpg_options['wp-greet-linesperpage'] = "10";
-      add_option("wp-greet-linesperpage",$wpg_options['wp-greet-linesperpage'],
-		 "lines on each page on log page","yes");
-    }; 
-
-    if ($wpg_options['wp-greet-usesmtp'] == "") {
-      $wpg_options['wp-greet-usesmtp'] = "1";
-      add_option("wp-greet-usesmtp",$wpg_options['wp-greet-usesmtp'],
-		 "which mail transfer method to use smtp=1, php mail=0","yes");
-    };   
+     
 }
 
 function wp_greet_deactivate()
 {
-  $options = array("wp-greet-minseclevel", 
-		   "wp-greet-captcha", 
-		   "wp-greet-mailreturnpath", 
-		   "wp-greet-autofillform",
-		   "wp-greet-bcc",
-		   "wp-greet-imgattach",
-		   "wp-greet-default-title",
-		   "wp-greet-default-header",
-		   "wp-greet-default-footer",
-		   "wp-greet-imagewidth",
-		   "wp-greet-logging",
-		   "wp-greet-gallery",
-		   "wp-greet-smilies",
-		   "wp-greet-formpage",
-		   "wp-greet-galarr",
-		   "wp-greet-linesperpage",
-		   "wp-greet-usesmtp",
-		   "wp-greet-stampimage",
-		   "wp-greet-stamppercent");
-
-  reset($options);
-  while (list($key,$val) = each($options)) {
-    delete_option($val);
-  }
+    
+    return; // avoid deletion of opts
+    
+    $options = array("wp-greet-minseclevel", 
+		     "wp-greet-captcha", 
+		     "wp-greet-mailreturnpath", 
+		     "wp-greet-autofillform",
+		     "wp-greet-bcc",
+		     "wp-greet-imgattach",
+		     "wp-greet-default-title",
+		     "wp-greet-default-header",
+		     "wp-greet-default-footer",
+		     "wp-greet-imagewidth",
+		     "wp-greet-logging",
+		     "wp-greet-gallery",
+		     "wp-greet-smilies",
+		     "wp-greet-formpage",
+		     "wp-greet-galarr",
+		     "wp-greet-linesperpage",
+		     "wp-greet-usesmtp",
+		     "wp-greet-stampimage",
+		     "wp-greet-stamppercent",
+		     'wp-greet-touswitch',
+		     'wp-greet-termsofusage',
+		     'wp-greet-mailconfirm',
+		     'wp-greet-mcduration',
+		     'wp-greet-mctext',
+		     'wp-greet-onlinecard',
+		     'wp-greet-ocduration',
+		     'wp-greet-octext',
+		     'wp-greet-logdays',
+		     'wp-greet-carddays',
+		     'wp-greet-fields');
+    
+    reset($options);
+    while (list($key,$val) = each($options)) {
+	delete_option($val);
+    }
 }
 
 ?>
--- wp-greet.php
+++ wp-greet.php
@@ -3,12 +3,12 @@
 Plugin Name: wp-greet
 Plugin URI: http://www.tuxlog.de
 Description: wp-greet is a wordpress plugin to send greeting cards from your wordpress blog.
-Version: 1.5
+Version: 3.7
 Author: Barbara Jany, Hans Matzen <webmaster at tuxlog.de>
 Author URI: http://www.tuxlog.de
 */
 
-/*  Copyright 2008,2009  Barbara Jany, Hans Matzen  (email : webmaster at tuxlog dot de)
+/*  Copyright 2008-2013  Barbara Jany, Hans Matzen  (email : webmaster at tuxlog dot de)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -29,32 +29,28 @@ if(preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) { die('You
 are not allowed to call this page directly.'); }
 
 
-define( "WP_GREET_VERSION", "1.4" );
+define( "WP_GREET_VERSION", "3.6" );
 
 // global options array
 $wpg_options = array();
 
 // include setup functions
-require_once("setup.php");
+$plugin_prefix_root = plugin_dir_path( __FILE__ );
+$plugin_prefix_filename = "{$plugin_prefix_root}/setup.php";
+include_once $plugin_prefix_filename;
+//require_once("setup.php");
 // include functions
 require_once("wpg-func.php");
+require_once("wpg-func-mail.php");
 // include admin options page
 require_once("wpg-admin.php");
 require_once("wpg-admin-log.php");
 require_once("wpg-admin-gal.php");
+require_once("wpg-admin-sec.php");
 
 // include form page
 require_once("wpg-form.php");
 
-//
-// just return the css link
-// this function is called via the wp_head hook
-//
-function wp_greet_css() {
-
-  $plugin_path = get_settings('siteurl') . '/wp-content/plugins/wp-greet';
-  echo "<link rel=\"stylesheet\" href=\"". $plugin_path. "/wp-greet.css\" type=\"text/css\" media=\"screen\" />\n";
-}
 
 function wp_greet_init()
 {
@@ -63,11 +59,34 @@ function wp_greet_init()
   $wpg_options=wpgreet_get_options();
 
   // add css in header
-  add_action('wp_head', 'wp_greet_css');
-
+  wp_enqueue_style("wp-greet", plugins_url('wp-greet.css', __FILE__) );
+  
+  // add thickbox for frontend
+  add_action('wp_print_scripts', 'wpg_add_thickbox_script');
+  add_action('wp_print_styles',  'wpg_add_thickbox_style' );
+  
+  
+  // add actions for future send
+  add_action("wpgreet_sendcard_link","cron_sendGreetCardLink",10,7);
+  add_action("wpgreet_sendcard_mail","cron_sendGreetCardMail",10,9);
+  
+  // add jquery extensions for datepicker if applicable
+  if ($wpg_options['wp-greet-future-send'] and !is_admin()) {
+  	wp_enqueue_script('jquery'); 
+    wp_enqueue_script('jquery-ui-wpgcustom', plugins_url('wp-greet/dtpicker/jquery-ui-1.10.0.custom.min.js', dirname(__FILE__)),array('jquery'));  	
+    
+    wp_enqueue_script('jquery-ui-timepicker', plugins_url('wp-greet/dtpicker/jquery-ui-timepicker-addon.js', dirname(__FILE__)),array('jquery','jquery-ui-wpgcustom'));
+	$locale=trim(substr(get_locale(),0,2)); 
+	wp_enqueue_script('jquery-ui-timepicker-i18n', plugins_url("wp-greet/dtpicker/i18n/jquery-ui-timepicker-$locale.js", dirname(__FILE__)),array('jquery-ui-timepicker'));
+	wp_enqueue_script('jquery-ui-datepicker-i18n', plugins_url("wp-greet/dtpicker/i18n/jquery.ui.datepicker-$locale.js", dirname(__FILE__)),array('jquery-ui-timepicker'));
+    
+	wp_enqueue_style('jquery-ui-wpgcustom-css', plugins_url('wp-greet/dtpicker/jquery-ui-1.10.0.custom.min.css'));
+		      	
+  }
+  
+  
   // Action calls for all functions 
   add_filter('the_content', 'searchwpgreet');
-  //add_filter('the_excerpt', 'searchwpgreet');
 
   // filter for ngg integration
   if ( $wpg_options['wp-greet-gallery']=="ngg") {
@@ -75,8 +94,15 @@ function wp_greet_init()
     // next line up to ngg-version 0.99 
     //add_filter('ngg_create_gallery_thumbcode', 'ngg_remove_thumbcode',2,2); 
     // next line from ngg-version 1.0 on 
-    add_filter('ngg_get_thumbcode', 'ngg_remove_thumbcode',2,2); 
+    add_filter('ngg_get_thumbcode', 'ngg_remove_thumbcode',2,2);  
   }
+  
+  // filter for wp integration
+  if ( $wpg_options['wp-greet-gallery']=="wp") { 
+  	remove_shortcode( 'gallery' );
+  	add_shortcode( 'gallery', 'wpgreet_gallery_shortcode' );
+  }
+
 }
 
 function wpg_add_menus()
@@ -84,32 +110,59 @@ function wpg_add_menus()
   $PPATH=ABSPATH.PLUGINDIR."/wp-greet/";
 
   // get translation 
-  $locale = get_locale();
-  if ( empty($locale) )
-    $locale = 'en_US';
-  if(function_exists('load_textdomain')) 
-    load_textdomain("wp-greet",ABSPATH . "wp-content/plugins/wp-greet/lang/".$locale.".mo");
+  load_plugin_textdomain('wp-greet',false,dirname( plugin_basename( __FILE__ ) ) . "/lang/");
   
-  add_menu_page('wp-greet','wp-greet', 8, $PPATH."wpg-admin.php","wpg_admin_form", site_url("/wp-content/plugins/wp-greet") . '/wp-greet.png');
+  add_menu_page('wp-greet','wp-greet', 'manage_options', $PPATH."wpg-admin.php","wpg_admin_form", site_url("/wp-content/plugins/wp-greet") . '/wp-greet.png');
 
-  add_submenu_page( $PPATH."wpg-admin.php", __('Galleries',"wp-greet"), __('Galleries', "wp-greet"), 8, $PPATH."wpg-admin-gal.php", "wpg_admin_gal") ;
+  add_submenu_page( $PPATH."wpg-admin.php", __('Galleries',"wp-greet"), __('Galleries', "wp-greet"), 'manage_options', $PPATH."wpg-admin-gal.php", "wpg_admin_gal") ;
 
-  add_submenu_page( $PPATH."wpg-admin.php", __('Logging',"wp-greet"), __('Logging', "wp-greet"), 8, $PPATH."wpg-admin-log.php", "wpg_admin_log") ;
+  add_submenu_page( $PPATH."wpg-admin.php", __('Security',"wp-greet"), __('Security', "wp-greet"), 'manage_options', $PPATH."wpg-admin-sec.php", "wpg_admin_sec") ;
 
+  add_submenu_page( $PPATH."wpg-admin.php", __('Logging',"wp-greet"), __('Logging', "wp-greet"), 'manage_options', $PPATH."wpg-admin-log.php", "wpg_admin_log") ;
+
+}
+
+// add thickbox to page headers
+function wpg_add_thickbox_script()
+{
+    wp_enqueue_script( 'thickbox' );
+}
+
+// add thickbox to page headers
+function wpg_add_thickbox_style()
+{
+    wp_enqueue_style( 'thickbox');
+}
+
+// wrapper functions for wp_cron trigger
+ function cron_sendGreetCardMail($sender,$sendername,$recv,$recvname,$title,
+			   $msgtext,$picurl,$ccsender,$debug=false) 
+{ 
+	//echo "###" . $sender .$sendername.$recv.$recvname.$title.$msgtext.$picurl.$ccsender.$debug."###";
+	sendGreetcardMail($sender,$sendername,$recv,$recvname,$title,$msgtext,$picurl,$ccsender,$debug);
+	log_greetcard($recv,addslashes($sender),$picurl,$msgtext);
+}
+
+function cron_sendGreetCardLink($sender,$sendername,$recv, $recvname,$duration, $fetchcode, $debug=false) 
+{ 
+	//echo "###".$sender.$sendername,$recv. $recvname.$duration. $fetchcode. $debug."###";
+	sendGreetcardLink($sender,$sendername,$recv, $recvname,$duration, $fetchcode, $debug);
+	mark_sentcard($fetchcode); 
 }
 
 //
 // MAIN
 //
-// activating deactivating the plugin
 register_activation_hook(__FILE__,'wp_greet_activate');
-//register_deactivation_hook(__FILE__,'wp_greet_deactivate');
+register_deactivation_hook(__FILE__,'wp_greet_deactivate');
 
+
+// add admin notice for broken NGG >= 2.0.0
+add_action('admin_notices','wpg_fix_broken_ngg_hint');
 // add admin menu 
 add_action('admin_menu', 'wpg_add_menus');
 
 // init plugin
 add_action('init', 'wp_greet_init');
- 
 
 ?>
